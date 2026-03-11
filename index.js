a52684a176085e7db3b51926c16d9bfd18cfc22e
PLacek7777 <cieszynski.rd@gmail.com>
2026-03-11 23:04:03 +0100

    Dodane komentarze do js i struktura plików json w readme

--- index.js
+++ index.js
@@ -11,25 +11,32 @@ const checkButton = document.getElementById("checkAnswer");
 const map = document.getElementById("map");
 const divmap = document.getElementById("divmap");
 
+//Czy nie znajdujemy się na stacji menu
 if (siteId != "menu") {
+  
+  //Wczytanie pliku json
   fetch(`media/questions-${siteId}.json`)
   .then(response => response.json())
   .then(data => {
-    questions = data["questions"];
-    tips = data["tips"];
+    questions = data["questions"]; //Pytań
+    tips = data["tips"]; //Zakończenia
     if(document.cookie.split(` ${siteId}=`).pop().split(";").shift() == 1){
       end();
     }
-    showQuestion();
+    showQuestion(); //Pokazuje pierwsze pytanie danej stacji
   })
+  //Error wczytywania
   .catch(error => {
     console.error("Błąd podczas ładowania pytań:", error);
     questionElement.textContent = "Nie udało się wczytać danych.";
   });
 }
+
+//Jeśli znajduemy się na staji menu wyświetla help
 else if (siteId == "menu") {
   displayHelp();
 }
+
 
 function end() {
   document.getElementById("question-box").innerHTML = `
@@ -52,12 +59,13 @@ function end() {
       finally{}
 }
 
+
 function showQuestion() {
   const q = questions[currentQuestion];
-  try{
+  try{ //Sprawdzanie tagu pytania
     switch(q.tag){
       case "trivia":
-        questionElement.innerHTML = q.text.replace(/\n/g, '<br>'); // Replace \n with <br>
+        questionElement.innerHTML = q.text.replace(/\n/g, '<br>'); // Zamienia \n na <br>
         answerInput.style.display = 'none';
         checkButton.innerHTML = 'Dalej';
         checkButton.onclick = checkAnswer;
@@ -65,7 +73,7 @@ function showQuestion() {
         feedback.style.color = '';
         break;
       case "question": 
-        questionElement.innerHTML = `Pytanie: ${q.text.replace(/\n/g, '<br>')}`; // Replace \n with <br>
+        questionElement.innerHTML = `Pytanie: ${q.text.replace(/\n/g, '<br>')}`; // Zamienia \n na <br>
         answerInput.style.display = 'block';
         checkButton.innerHTML = 'Sprawdź odpowiedź';
         checkButton.onclick = checkAnswer;
@@ -74,7 +82,7 @@ function showQuestion() {
         feedback.style.color = '';
         break;
       case "passwd": 
-      questionElement.innerHTML = `${q.text.replace(/\n/g, '<br>')}`; // Replace \n with <br>
+      questionElement.innerHTML = `${q.text.replace(/\n/g, '<br>')}`; // Zamienia \n na <br>
       answerInput.placeholder = "Wpisz hasło...";
       answerInput.style.display = 'block';
       checkButton.innerHTML = 'Sprawdź hasło';
@@ -85,18 +93,13 @@ function showQuestion() {
       break;
     }
   }
+
   finally{
     console.log("error");
   }
 }
-// Pokazywanie stacji
-function continueToNext() {
-  const text = document.getElementById("text");
-  text.innerHTML = `Oto wskazówka do pierwszej stacji: Na dole schodów. Hasło: Alleluja`; // TODO: Add a tip for the first station
-  map.style.display = 'block';
-  divmap.style.display = 'block'; 
-}
 
+//Zabezpieczenie hasła
 async function hash(tekst) {
   const encoder = new TextEncoder();
   const data = encoder.encode(tekst);
@@ -108,27 +111,31 @@ async function hash(tekst) {
     .join('');
 }
 
+
 function checkAnswer() {
-  const userAnswer = answerInput.value.trim().toLowerCase();
-  const correctAnswers = questions[currentQuestion].answers;
+  const userAnswer = answerInput.value.trim().toLowerCase(); //przycina i obniża udzieloną odpowiedź
+  const correctAnswers = questions[currentQuestion].answers; //pobiera poprawne odpowiedzi
   const tag = questions[currentQuestion].tag;
   
+  //sprawdzenie poprawdności odpowiedzi
   if (correctAnswers.includes(userAnswer) || tag == "trivia") {
     currentQuestion++;
     if (currentQuestion < questions.length) {
-      showQuestion();
+      showQuestion(); //pokazanie kolejnego pytania
     } 
     else {
       document.cookie = `${siteId}=1`;
-      end();
+      end(); //jeśli nie ma już pytań pokaż zakończenie
     }
   } 
+  //błędna odpowiedź
   else {
     feedback.textContent = "❌ To nie to. Spróbuj ponownie.";
     feedback.style.color = "#e74c3c";
   }
 }
 
+//sprawdzenie hasła 
 function checkPassword() {
   const userAnswer = answerInput.value.trim().toLowerCase();
   const correctAnswers = questions[currentQuestion].answers;
@@ -138,6 +145,7 @@ function checkPassword() {
       currentQuestion++;
       showQuestion();
     }
+    //niepoprawne hasło
     else {
       feedback.textContent = "❌ To nie to. Spróbuj ponownie.";
       feedback.style.color = "#e74c3c";
@@ -145,6 +153,7 @@ function checkPassword() {
   });
 }
 
+//pokazuję menu help (to samo co na początku)
 function displayHelp() {
   fetch("media/help.json")
   .then(response => response.json())
@@ -170,3 +179,13 @@ function displayHelp() {
 }
 
 showQuestion();
+
+//---------------------------Dla index.html
+
+//Wskazówka do pierwszej stacji
+function continueToNext() {
+  const text = document.getElementById("text");
+  text.innerHTML = `Oto wskazówka do pierwszej stacji: Na dole schodów. Hasło: Alleluja`;
+  map.style.display = 'block';
+  divmap.style.display = 'block'; 
+}
--- readme.txt
+++ readme.txt
@@ -25,3 +25,18 @@ q3
 q4
   "answers": ["liturgia słowa"]
   "answers": ["hosanna"]
+
+Każda stajca to osobna podstrona z własnym plikiem html i json. Kożystają z jednego pliku css i jednego js.
+
+Każdy body ma id oznaczające na jakiej stacji jesteśmy: [menu, q1, q2, q3] odpowiadające plikowi json
+np: stacja 3: q3 => questions-q3.json
+
+Plik json składa się z dwóch częsci
+  1. questions zawierające kolejne pytania/informacje do wyświetlenia oznaczone tagami
+    a. passwd - hasło z poprzedniej stacji
+    b. question - pytanie i odpowiedzi na nie
+    c. trivia - informacje/ciekawostki do wyświetlenia
+  2. tips zawiera informacje jak dostać się do kolejnej stacji oraz zadaie do wypełnienia w drodze
+
+Plik help ma inną strukturę
+Każdy wpis składający się z tytułu i treści to jeden punkt w menu pomocy dostępnego pod ptajnikiem (prawy dolny róg strony)
